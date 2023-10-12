a721896e0dfd217b08db613ddd0a04192651155a
Alan Galavis <102612482+trabajosUPC-AlanGalavis@users.noreply.github.com>
2023-10-12 13:02:11 -0500

    feat(button-primary): Added button primary component

---
+++ src/credit-car/components/button-primary.component.vue
@@ -0,0 +1,28 @@
+<script>
+export default {
+  name: 'button-primary',
+  props: {
+    text: null,
+    buttonColor: null,
+    buttonTextColor: null,
+    buttonBorderColor: null,
+  },
+};
+</script>
+
+<template>
+  <pv-button class="button-primary" :style="{ backgroundColor: buttonColor, borderColor: buttonBorderColor}">
+    <p :style="{ color: buttonTextColor }">{{ text }}</p>
+  </pv-button>
+</template>
+
+<style scoped>
+.button-primary {
+  justify-content: center;
+  align-items: center;
+  border: solid 2px;
+  border-radius: 50px;
+  box-shadow: 0 4px 4px 0 rgba(0, 0, 0, 0.25);
+  font-family: var(--font-primary);
+}
+</style>
